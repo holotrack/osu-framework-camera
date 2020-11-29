1c652a1f78548d53f7e71afc86187b68f90cdb90
Nitrous <n20gaming2000@gmail.com>
2020-11-29 19:35:28 +0800

    Add project files.

---
+++ osu.Framework.Camera/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osu.Framework.Camera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
