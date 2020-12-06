28962b42d474c8d2fd97a37c27f66e92eaaab0cc
Nitrous <n20gaming2000@gmail.com>
2020-12-06 12:44:29 +0800

    add tests for CameraVirtual

---
+++ osu.Framework.Camera.Tests/CameraVirtualTest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under MIT. See LICENSE for details.
+
+using System.IO;
+using NUnit.Framework;
+
+namespace osu.Framework.Camera.Tests
+{
+    [TestFixture]
+    public class CameraVirtualTest
+    {
+        [Test]
+        public void TestFileCaching()
+        {
+            var camera = new TestCameraVirtual(new MemoryStream());
+            Assert.IsTrue(File.Exists(camera.FilePath));
+
+            camera.Dispose();
+            Assert.IsFalse(File.Exists(camera.FilePath));
+        }
+    }
+}
---
+++ osu.Framework.Camera.Tests/TestCameraVirtual.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under MIT. See LICENSE for details.
+
+using System.IO;
+
+namespace osu.Framework.Camera.Tests
+{
+    internal class TestCameraVirtual : CameraVirtual
+    {
+        public new string FilePath => base.FilePath;
+
+        public new DecoderState State => base.State;
+
+        public TestCameraVirtual(Stream stream)
+            : base(stream)
+        {
+        }
+    }
+}
